c3794a5f01effaea908e7dc667b6a9d0ff821243
gaby <gabriela.leyton@correoaiep.cl>
2024-10-02 23:05:18 -0300

    02/10

--- app/src/main/java/com/example/tolist_de_aprendizaje/ActivityMenu.java
+++ app/src/main/java/com/example/tolist_de_aprendizaje/ActivityMenu.java
@@ -1,14 +1,12 @@
 package com.example.tolist_de_aprendizaje;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
-
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ActivityMenu extends AppCompatActivity {
 
@@ -20,26 +18,31 @@ public class ActivityMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        vf = (ViewFlipper) findViewById(R.id.slider);
+        vf = findViewById(R.id.slider);
 
-        for (int i = 0; i < image.length; i++)
-        {
+        for (int i = 0; i < image.length; i++) {
             flip_image(image[i]);
         }
 
         vf.setFlipInterval(2800);
         vf.setAutoStart(true);
+        vf.setInAnimation(this, android.R.anim.slide_in_left);
+        vf.setOutAnimation(this, android.R.anim.slide_out_right);
 
-        vf.setInAnimation( this, android.R.anim.slide_in_left);
-        vf.setOutAnimation( this, android.R.anim.slide_out_right);
+
+        Button DesaBtn = findViewById(R.id.DesaBtn);
+        DesaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityMenu.this, Activity_IyA.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void flip_image(int i)
-    {
-        ImageView view = new ImageView( this);
+    public void flip_image(int i) {
+        ImageView view = new ImageView(this);
         view.setBackgroundResource(i);
         vf.addView(view);
-
     }
-
 }
